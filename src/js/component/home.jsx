849ab1e973cc88e6047d0b464203bf1f6150e231
Nachocordoba13 <nachocordoba34@gmail.com>
2024-11-25 20:43:15 +0000

    Lista de tareas

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue,] = useState("");
+	const [tareas, setTareas] = useState([]);
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1 className="text-center mt-5">Mis Tareas</h1>
+			<ul>
+			<li><input type="text" 
+			onChange={(e) => setInputValue(e.target.value)}
+			value={inputValue}
+			onKeyDown={(e) => {
+				if (e.key === "Enter") {
+					setTareas(tareas.concat(inputValue));
+					setInputValue("")
+				}
+			}}
+			placeholder="Escribe aquí tu tarea"></input></li>
+				{tareas.map((item, index) => (
+					<li>
+						{item} <i class="fas fa-trash-alt" onClick={() => setTareas(tareas.filter((t, currentIndex) => index != currentIndex))}></i>
+					</li>
+				))}
+			</ul>
+			<div>{tareas.length} tareas</div>
 		</div>
 	);
 };
